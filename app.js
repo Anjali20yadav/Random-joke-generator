daa4c0aaa1d511ec36253a8152baa9ab86f77740
Anjali20yadav <anjali.kmri2541@gmail.com>
2024-10-05 11:10:51 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,12 @@
+const jokecontainer=document.getElementById("joke");
+const btn=document.getElementById("btn");
+const url="https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,religious,racist,sexist,explicit&type=single";
+let getjoke=()=>{
+    fetch(url)
+    .then(data=>data.json())
+    .then(item=>{
+        jokecontainer.textContent=`${item.joke}`;
+});
+}
+btn.addEventListener("click",getjoke);
+getjoke();
